b03d4ca16e0a8cd6052d8034f651bb65d61d2507
Mikhail Shytsko <xfiive@mail.ru>
2024-08-07 00:23:11 +0300

    initial commit

---
+++ src/main/java/org/vsservice/vsservice/VsserviceApplication.java
@@ -0,0 +1,13 @@
+package org.vsservice.vsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/vsservice/vsservice/VsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vsservice.vsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
