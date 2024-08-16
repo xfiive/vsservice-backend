a235d74cae63323bf6898bc0f65e0e8f575404b3
Mikhail Shytsko <xfiive@mail.ru>
2024-08-16 22:58:25 +0530

    Added caching configuration

--- src/main/java/org/vsservice/vsservice/VsserviceApplication.java
+++ src/main/java/org/vsservice/vsservice/VsserviceApplication.java
@@ -2,10 +2,12 @@ package org.vsservice.vsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableRetry
+@EnableCaching
 public class VsserviceApplication {
 
     public static void main(String[] args) {
