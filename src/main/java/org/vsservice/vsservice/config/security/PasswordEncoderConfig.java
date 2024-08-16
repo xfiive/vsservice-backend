7529c07a8b923aed329f2de8d7b1cf29b6b6e79f
Mikhail Shytsko <xfiive@mail.ru>
2024-08-16 22:59:19 +0530

    Updated project structure

--- src/main/java/org/vsservice/vsservice/config/security/PasswordEncoderConfig.java
+++ src/main/java/org/vsservice/vsservice/config/security/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package org.vsservice.vsservice.config;
+package org.vsservice.vsservice.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
